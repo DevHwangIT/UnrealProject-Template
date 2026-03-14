b8f951528a6b1a5bbf144a46ab4925e096dc0edc
Dev.Hwang <zxc1063@naver.com>
2026-03-15 07:13:55 +0900

    Migration Works 5.7 Unreal Editor Version

---
+++ Source/Template_Project/Template_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Template_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Template_Project, "Template_Project" );
